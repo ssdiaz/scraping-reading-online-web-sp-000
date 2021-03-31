d42687b2ef1a839579c1e03bec6ff520f3a965b1
Sam D <samidiaz3@gmail.com>
2021-03-31 20:59:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN").text
+
+#puts doc.css(".headline-26OIBN")
+#puts doc.css(".headline-26OIBN").text.strip
